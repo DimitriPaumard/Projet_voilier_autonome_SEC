4c39d319c77f0fa15dfa5a3833a872aaecbfffcb
Youn-Perron <younperron@live.fr>
2023-01-17 10:54:12 +0100

    Add VoilierLibs folder & subfolders
    --

---
+++ Voilier_cubeIDE_proj/VoilierLibs/Inc/example.hpp
@@ -0,0 +1,8 @@
+/*
+ * example.hpp
+ *
+ *  Created on: Jan 17, 2023
+ *      Author: youn
+ */
+
+#include "stm32f4xx_hal.h"
---
+++ Voilier_cubeIDE_proj/VoilierLibs/Src/example.cpp
@@ -0,0 +1,8 @@
+/*
+ * example.cpp
+ *
+ *  Created on: Jan 17, 2023
+ *      Author: youn
+ */
+
+#include "example.hpp"
